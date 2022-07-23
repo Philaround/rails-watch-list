45aed8ac3b782ec50c349f9b473917cb698f6eef
Filippos Lavrentidis <n.p.lavrentidis@gmail.com>
2022-07-23 13:32:16 +0300

    models finalized

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :lists, only: [:index, :create, :new, :show] do
-    resourses :bookmarks, only: [:create, new, :destroy]
+    resources :bookmarks, only: [:create, :new, :destroy]
   end
-
-
 end
